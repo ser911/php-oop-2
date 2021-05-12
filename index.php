9ee2c52a8539b86a2761a455fa03b5aac6ac722a
Sergio <sergio.marretta@libero.it>
2021-05-12 17:56:24 +0200

    construct

--- index.php
+++ index.php
@@ -1,37 +1,35 @@
 <?php
-
+//attributi
 class product{
 private $item;
 public $price;
 public $brand;
 public $color;
 
-public function setItem($_item){
-    if (strlen($_item) > 3 ){
+//costruttore
+public function __construct($_item,$_price,$_brand,$_color){
+$this->item = $_item;
+$this->price = $_price;
+$this->brand = $_brand;
+$this->color = $_color;
+
+}
+//metodi
+
+    public function setItem($_item){
+        if (strlen($_item) > 3){
         $this->item = $_item;
+    }
+
+  }
+
 }
-}
+$product_1 = new Product("Screen","240","Lenovo","Dark-grey");
 
-$product_1 = new Product();
+$product_2 = new Product("Keyboard","90","Samsung","White");
 
-$product_1 ->setItem("Screen");
-$product_1 -> price = "240";
-$product_1 -> brand = "Lenovo";
-$product_1 -> color = "Dark-grey";
+$product_3 = new Product("Optical mouse","65","Toshiba","Black");
 
-
-$product_2 = new Product();
-$product_2 ->setItem("Keyboard");
-$product_2 -> price = "90";
-$product_2 -> brand = "Samsung";
-$product_2 -> color = "White";
-
-
-$product_3 = new Product();
-$product_3 ->setItem("Optical mouse");
-$product_3 -> price = "65";
-$product_3 -> brand = "Toshiba";
-$product_3 -> color = "Black";
 
 var_dump($product_1);
 var_dump($product_2);
