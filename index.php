248c9b8ba9f4a4ca53f2f4b3a71a8f24bc67c040
Sergio <sergio.marretta@libero.it>
2021-05-12 16:55:27 +0200

    if

--- index.php
+++ index.php
@@ -7,26 +7,28 @@ public $brand;
 public $color;
 
 public function setItem($_item){
-    $this->item = $_item;
+    if (strlen($_item) > 3 ){
+        $this->item = $_item;
 }
 }
 
 $product_1 = new Product();
-$product_1 -> item = "Screen";
+
+$product_1 ->setItem("Screen");
 $product_1 -> price = "240";
 $product_1 -> brand = "Lenovo";
 $product_1 -> color = "Dark-grey";
 
 
 $product_2 = new Product();
-$product_2 -> item = "Keyboard";
+$product_2 ->setItem("Keyboard");
 $product_2 -> price = "90";
 $product_2 -> brand = "Samsung";
 $product_2 -> color = "White";
 
 
 $product_3 = new Product();
-$product_3 -> item = "Optical-mouse";
+$product_3 ->setItem("Optical mouse");
 $product_3 -> price = "65";
 $product_3 -> brand = "Toshiba";
 $product_3 -> color = "Black";
